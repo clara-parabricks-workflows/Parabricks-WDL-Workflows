d02cbd9c4b68c25c068512c9a0332da22c71c999
Gary Burnett <gburnett@nvidia.com>
2025-12-11 10:32:10 -0800

    Tested download_data.sh

--- workflows/fq2bam/download_data.sh
+++ workflows/fq2bam/download_data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash 
 
-echo "Downloading fq2bam data" 
+# This script downloads the data files for fq2bam into $DATA_DIR
+# It resumes interrupted file downloads and doesn't download files if they already exist
+
+DATA_DIR=../../data
+mkdir -p ${DATA_DIR}
+
+file_url="https://github.com/nf-core/test-datasets/raw/methylseq/testdata/SRR389222_sub1.fastq.gz"
+
+curl $file_url -C - -O --output-dir ${DATA_DIR}
