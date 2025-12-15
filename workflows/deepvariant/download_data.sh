bd74590fa7f54fa70c228f159ab263d1cdf90da8
Gary Burnett <gburnett@nvidia.com>
2025-12-15 10:03:03 -0800

    Updating data download script for deepvariant

--- workflows/deepvariant/download_data.sh
+++ workflows/deepvariant/download_data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash 
 
-echo "Downloading deepvariant data" 
+# This script downloads the data files for fq2bam into $DATA_DIR
+# It resumes interrupted file downloads and doesn't download files if they already exist
+
+DATA_DIR=../../data
+mkdir -p ${DATA_DIR}
+
+file_url="https://raw.githubusercontent.com/nf-core/test-datasets/modules/data/genomics/homo_sapiens/illumina/bam/test2.paired_end.recalibrated.sorted.bam"
+
+curl $file_url -C - -o ${DATA_DIR}/test.bam
